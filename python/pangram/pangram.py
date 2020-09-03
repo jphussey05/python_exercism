d9ff7b28a47829633724f8ace1def907c1e45225
Jason Hussey <jphussey05@gmail.com>
2020-09-02 23:49:31 -0600

    added lower

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,5 @@
 def is_pangram(sentence):
     alphas = set('abcdefghijklmnopqrstuvwxyz')
-    diff = alphas - set(sentence)
+    diff = alphas - set(sentence.lower())
 
     return True if len(diff) == 0 else False
